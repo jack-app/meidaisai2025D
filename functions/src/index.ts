41293e2b2498cc0a1af555c30710c86b84c7f9bf
hbenpitsu <hbenpitsu15@gmail.com>
2025-05-11 20:47:10 +0900

    ルーティングの変更

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,19 +7,32 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { setGlobalOptions } from "firebase-functions";
-
-setGlobalOptions({
-    region: "asia-northeast1"
-})
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+import * as logger from "firebase-functions/logger";
+
+// ルータとしてexpressを使用
+// ルータ: URL（エンドポイント）に基づいて返すレスポンスを決定するもの．
+import express from "express";
+
+// Webページのエンドポイント
+const app = express();
+
+app.get("/", (req, res)=> {
+    logger.log("Accessing the game page.");
+    res.send("You are successfully accessing the game Page.");
+})
+
+app.get("/api/", (req, res)=> {
+    logger.log("Accessing the game api.");
+    res.send("You are successfully accessing the game API.");
+})
+
+// Firebase Functionsのデプロイ設定
+import {onRequest} from "firebase-functions/v2/https";
+
+export const appFunction = onRequest( // プログラムを東京で作動させる
+    {region: "asia-northeast1"},
+    app
+);
